0a19fc2d43f97498544df2dd1cdb07105183657e
Deana <demaisja@gmail.com>
2023-10-03 19:17:46 -0700

    js logic for identifying palindromes is complete but with console.log debugging and messaging. Need to develop how to message the user and also how to only collect the final input

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,37 @@
-const elem = document.querySelector('input');
+const elem = document.querySelector("input");
 
-elem.addEventListener('input', handleInput);
+elem.addEventListener("input", handleInput);
+
+function handleInput(e) {
+  if (e.target.value < 0) {
+    console.log("That's negative");
+    e.target.value = 0;
+  } else if (e.target.value < 10) {
+    console.log("That's a palindrome!");
+  } else {
+    let query = e.target.value;
+    let text = query.toString();
+    let textLen = text.length;
+
+    if (textLen % 2 === 0 && query % 11 === 0) {
+      console.log("That's a palindrome");
+    } else {
+      //easy cases over, now we will convert the number to an array and reverse it
+      console.log("fell to this case");
+      const array = [...text];
+      console.log(array);
+      let arrayText = array.toString();
+      console.log(arrayText);
+      const reversedArray = array.reverse();
+      console.log(reversedArray);
+      let reversedText = reversedArray.toString();
+      console.log(reversedText);
+
+      if (arrayText === reversedText) {
+        console.log("Yes, this is a complicated Palindrome!");
+      } else {
+        console.log("Nope, this is not a palindrome.");
+      }
+    }
+  }
+}
