6972a6d17fac59cae17452c0116a5829c5597a33
Deana <demaisja@gmail.com>
2023-10-04 16:25:05 -0700

    html, bootstrap finalized, wave checked html verified

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -11,6 +11,7 @@ function handleInput(elem) {
   //check for single digits
   else if (elem.target.value < 10) {
     document.getElementById("result").innerHTML = "Yes, this is a palindrome!";
+    document.getElementById("result").style.color = "green";
   }
   //check all positive numbers 2 digits or greater
   else {
@@ -22,6 +23,7 @@ function handleInput(elem) {
     if (textLen % 2 === 0 && query % 11 === 0) {
       document.getElementById("result").innerHTML =
         "Yes, this is a palindrome!";
+      document.getElementById("result").style.color = "green";
     }
     //easy cases over, now we will convert the number to an array and reverse it
     else {
@@ -33,10 +35,24 @@ function handleInput(elem) {
       if (arrayText === reversedText) {
         document.getElementById("result").innerHTML =
           "Yes, this is a palindrome!";
+        document.getElementById("result").style.color = "green";
       } else {
         document.getElementById("result").innerHTML =
           "Nope, this is not a palindrome.";
+        document.getElementById("result").style.color = "red";
       }
     }
   }
 }
+/* 
+Outside sources 
+How To Get The Length of a String in JavaScript (w3schools.com)
+JavaScript Number toString() Method (w3schools.com)
+Comparing Arrays in JavaScript – How to Compare 2 Arrays in JS (freecodecamp.org)
+JavaScript Array reverse() Method (w3schools.com)
+String to Array in JavaScript - GeeksforGeeks
+Spread syntax (...) - JavaScript | MDN (mozilla.org)
+JavaScript Array toString() Method (w3schools.com)
+W3Schools Tryit Editor
+https://www.w3schools.com/js/js_htmldom_css.asp
+*/
