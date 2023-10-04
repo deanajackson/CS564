544377a12e7bf796c226fc7f88568b7e4e0cfda4
Deana <demaisja@gmail.com>
2023-10-04 15:25:06 -0700

    js finalized

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -2,35 +2,40 @@ const elem = document.querySelector("input");
 
 elem.addEventListener("input", handleInput);
 
-function handleInput(e) {
-  if (e.target.value < 0) {
-    console.log("That's negative");
-    e.target.value = 0;
-  } else if (e.target.value < 10) {
-    console.log("That's a palindrome!");
-  } else {
-    let query = e.target.value;
+function handleInput(elem) {
+  ///check for negative input
+  if (elem.target.value < 0) {
+    document.getElementById("result").innerHTML =
+      "That's a negative number. Please enter a positive number.";
+  }
+  //check for single digits
+  else if (elem.target.value < 10) {
+    document.getElementById("result").innerHTML = "Yes, this is a palindrome!";
+  }
+  //check all positive numbers 2 digits or greater
+  else {
+    let query = elem.target.value;
     let text = query.toString();
     let textLen = text.length;
 
+    //check if there are an even number of digits and divisible by 11
     if (textLen % 2 === 0 && query % 11 === 0) {
-      console.log("That's a palindrome");
-    } else {
-      //easy cases over, now we will convert the number to an array and reverse it
-      console.log("fell to this case");
+      document.getElementById("result").innerHTML =
+        "Yes, this is a palindrome!";
+    }
+    //easy cases over, now we will convert the number to an array and reverse it
+    else {
       const array = [...text];
-      console.log(array);
       let arrayText = array.toString();
-      console.log(arrayText);
       const reversedArray = array.reverse();
-      console.log(reversedArray);
       let reversedText = reversedArray.toString();
-      console.log(reversedText);
 
       if (arrayText === reversedText) {
-        console.log("Yes, this is a complicated Palindrome!");
+        document.getElementById("result").innerHTML =
+          "Yes, this is a palindrome!";
       } else {
-        console.log("Nope, this is not a palindrome.");
+        document.getElementById("result").innerHTML =
+          "Nope, this is not a palindrome.";
       }
     }
   }
