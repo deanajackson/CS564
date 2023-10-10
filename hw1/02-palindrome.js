77afac152a0fcd99c192f57b281fb151466b23bd
Deana <demaisja@gmail.com>
2023-10-09 19:33:12 -0700

    pr changes completed

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -4,14 +4,17 @@ elem.addEventListener("input", handleInput);
 
 function handleInput(elem) {
   ///check for negative input
-  if (elem.target.value < 0) {
-    document.getElementById("result").innerHTML =
+  const result = document.getElementById("result");
+  if (elem.target.value.toString() === "") {
+    result.innerHTML = "";
+  } else if (elem.target.value < 0) {
+    result.innerHTML =
       "That's a negative number. Please enter a positive number.";
   }
   //check for single digits
   else if (elem.target.value < 10) {
-    document.getElementById("result").innerHTML = "Yes, this is a palindrome!";
-    document.getElementById("result").style.color = "green";
+    result.innerHTML = "Yes, this is a palindrome!";
+    result.style.color = "green";
   }
   //check all positive numbers 2 digits or greater
   else {
@@ -21,25 +24,22 @@ function handleInput(elem) {
 
     //check if there are an even number of digits and divisible by 11
     if (textLen % 2 === 0 && query % 11 === 0) {
-      document.getElementById("result").innerHTML =
-        "Yes, this is a palindrome!";
-      document.getElementById("result").style.color = "green";
+      result.innerHTML = "Yes, this is a palindrome!";
+      result.style.color = "green";
     }
     //easy cases over, now we will convert the number to an array and reverse it
     else {
       const array = [...text];
-      let arrayText = array.toString();
+      const arrayText = array.toString();
       const reversedArray = array.reverse();
-      let reversedText = reversedArray.toString();
+      const reversedText = reversedArray.toString();
 
       if (arrayText === reversedText) {
-        document.getElementById("result").innerHTML =
-          "Yes, this is a palindrome!";
-        document.getElementById("result").style.color = "green";
+        result.innerHTML = "Yes, this is a palindrome!";
+        result.style.color = "green";
       } else {
-        document.getElementById("result").innerHTML =
-          "Nope, this is not a palindrome.";
-        document.getElementById("result").style.color = "red";
+        result.innerHTML = "Nope, this is not a palindrome.";
+        result.style.color = "red";
       }
     }
   }
