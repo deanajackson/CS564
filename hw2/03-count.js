437cb344d6371eff05151b23fb05f40816ea77e5
Deana <demaisja@gmail.com>
2023-10-17 12:45:03 -0700

    Ex3 implemented

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,29 @@
 // Add your code here
+const input = document.querySelector("input");
+let searchArray = [];
+//const mainTextArray = cleanText();
+let paragraph = document.querySelector("#paragraph");
 
-input.addEventListener('keydown', handleKeyDown);
+let text = document.getElementById("paragraph").innerHTML.toLowerCase();
+input.addEventListener("keydown", handleKeyDown);
+
+function handleKeyDown(e) {
+  if (e.key === "Backspace") {
+    e.target.value = "";
+    searchArray = [];
+  } else if (e.key === "Shift") {
+    let searchStr = searchArray.toString();
+    searchStr = searchStr.replaceAll(",", "");
+    findMatch(searchStr);
+  } else {
+    searchArray.push(e.key);
+  }
+}
+
+function findMatch(searchStr) {
+  text = text.replaceAll(
+    searchStr,
+    "<span class='highlight'>" + searchStr + "</span>"
+  );
+  paragraph.innerHTML = text;
+}
