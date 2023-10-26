b95a428ffc01e525bc2b29411c9a95c03fae874c
Deana <demaisja@gmail.com>
2023-10-25 21:46:44 -0600

    api data printing to console

--- hw3/01-characters.js
+++ hw3/01-characters.js
@@ -1,4 +1,23 @@
 // url for the Thrones API
-const url = 'https://thronesapi.com/api/v2/Characters';
+const url = "https://thronesapi.com/api/v2/Characters";
+let characters = [];
 
-fetch(url);
+const fetchData = (url) => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      //charaters.push;
+      data.forEach((character) => {
+        characters.push(character);
+        console.log(
+          character.firstName,
+          character.lastName,
+          character.fullName,
+          character.title
+        );
+      });
+    })
+    .catch((error) => console.error(error));
+};
+fetchData(url);
