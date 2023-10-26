fc0bf9eef102addb33c331097a8555b2a6f8ff57
Deana <demaisja@gmail.com>
2023-10-25 21:00:28 -0600

    form modifications based on PR feedback

--- hw2/05-form.js
+++ hw2/05-form.js
@@ -1,7 +1,6 @@
 // Add your code here
 const form = document.getElementById("form");
 
-document.getElementById("resetBtn").addEventListener("click", clearForm);
 let userName = document.getElementById("userName");
 let userEmail = document.getElementById("userEmail");
 let regStatus = document.getElementById("regStatus");
@@ -18,29 +17,6 @@ function logSubmit() {
   console.log(opSysChecked.checked);
   console.log(fsWebDevChecked.checked);
   console.log(otherInfo.value);
-}
-function clearForm() {
-  if (userName.value != null) {
-    userName.value = null;
-  }
-  if (userEmail.value != null) {
-    userEmail.value = null;
-  }
-  if (regStatus.value != null) {
-    regStatus.value = null;
-  }
-  if (progLangChecked.checked != false) {
-    progLangChecked.checked = false;
-  }
-  if (opSysChecked.checked != false) {
-    opSysChecked.checked = false;
-  }
-  if (fsWebDevChecked.checked != false) {
-    fsWebDevChecked.checked = false;
-  }
-  if (otherInfo.value != null) {
-    otherInfo.value = null;
-  }
 }
 
 form.addEventListener("submit", logSubmit);
